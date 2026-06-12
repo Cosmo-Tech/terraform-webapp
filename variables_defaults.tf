2a11202b2054bca335cec93d212c45bcb69e5e16
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2026-06-12 15:01:59 +0200

    chore: by default use latest stable webapp

--- variables_defaults.tf
+++ variables_defaults.tf
@@ -11,7 +11,7 @@ variable "cosmotechwebapp_chart_name" { default = "cosmotech-business-webapp" }
 variable "cosmotechwebapp_chart_repository" { default = "https://cosmo-tech.github.io/helm-charts" }
 variable "cosmotechwebapp_chart_tag" { default = "0.3.0" }
 variable "cosmotechwebapp_image_repository_server" { default = "ghcr.io/cosmo-tech/azure-sample-webapp" }
-variable "cosmotechwebapp_image_tag_server" { default = "v7.0.3-vanilla" }
+variable "cosmotechwebapp_image_tag_server" { default = "v7.0.4-vanilla" }
 variable "cosmotechwebapp_image_repository_functions" { default = "ghcr.io/cosmo-tech/azure-sample-webapp" }
-variable "cosmotechwebapp_image_tag_functions" { default = "v7.0.3-vanilla" }
+variable "cosmotechwebapp_image_tag_functions" { default = "v7.0.4-vanilla" }
 
